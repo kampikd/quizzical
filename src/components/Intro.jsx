d6fc3a9648688decaa6460bfc16d779916d12d18
Damian Kampik <damian.kampik@u2i.com>
2022-09-06 15:24:45 +0200

    Initial commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Intro() {
+    return (
+        <main className="intro">
+            <h1>Quizzical</h1>
+            <h3>The musical trivia game</h3>
+            <button className="intro--btn">
+                Start quiz
+            </button>
+        </main>
+    )
+}
